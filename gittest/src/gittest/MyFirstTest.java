8d8c108c4928b673d3b1c1f86f275a46aa76c4bd
scdeco <jacob@scdeco.com>
2015-05-11 09:58:43 -0700

    Added Jacob

--- gittest/src/gittest/MyFirstTest.java
+++ gittest/src/gittest/MyFirstTest.java
@@ -4,7 +4,7 @@ public class MyFirstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world,this is a test!");
+		System.out.println("Hello world,this is a test,jacob!");
 
 	}
 
