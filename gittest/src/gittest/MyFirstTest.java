3a09414482f2ba478ed252c5cc1dfbac0f711584
scdeco <jacob@scdeco.com>
2015-05-11 09:56:27 -0700

    change to this is a test

---
+++ gittest/src/gittest/MyFirstTest.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class MyFirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world,this is a test!");
+
+	}
+
+}
